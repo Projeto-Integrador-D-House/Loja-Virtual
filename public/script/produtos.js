4aeecd7ee136ffd42fa289b4cab6d6cd2a5222cc
SamaraBilby <samarabilby@hotmail.com>
2022-10-24 14:05:11 -0300

    adicionando lista de produtos em JS

---
+++ public/script/produtos.js
@@ -0,0 +1,100 @@
+const produtos = [
+    {
+        id: 1,
+        title: "Cyper kid infinite",
+        categoria: "jogos",
+        preco: 200.00,
+        img: '../public/image/produto_1.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 2,
+        title: "Chronosplit",
+        categoria: "jogos",
+        preco: 189.99,
+        img: '../public/image/produto_2.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 3,
+        title: "Ice Dome: Exile",
+        categoria: "jogos",
+        preco: 175.00,
+        img: '../public/image/produto_3.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+
+    {
+        id: 4,
+        title: "Ancient Souls",
+        categoria: "jogos",
+        preco: 190.00,
+        img: '../public/image/produto_4.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+
+    {
+        id: 5,
+        title: "Kira And the Fading Islands",
+        categoria: "jogos",
+        preco:  129.99,
+        img: '../public/image/produto_5.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 6,
+        title: "Dead At Last",
+        categoria: "jogos",
+        preco: 189.99,
+        img: '../public/image/produto_6.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 7,
+        title: "Minecraft",
+        categoria: "jogos",
+        preco: 229.99,
+        img: '../public/image/produto_7.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 8,
+        title: "Witcher 2",
+        categoria: "jogos",
+        preco: 175.99,
+        img: '../public/image/produto_8.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 9,
+        title: "Witcher 2",
+        categoria: "jogos",
+        preco: 1431.00,
+        img: '../public/image/produto_2.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 10,
+        title: "Jogo XXXX",
+        categoria: "jogo",
+        preco: 135.99,
+        img: '../public/image/produto_4.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 11,
+        title: "Jogo ZZZZ",
+        categoria: "jogo",
+        preco: 199.99,
+        img: '../public/image/produto_5.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    },
+    {
+        id: 12,
+        title: "Jogo WWWW",
+        categoria: "jogo",
+        preco: 276.99,
+        img: '../public/image/produto_6.webp',
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur accusantium nulla maiores quis. Quasi quo possimus exercitationem, repudiandae voluptatum odio magnam quam"
+    }
+];
