e4e3db24d17634e8d4335187a578541eff35e170
Luiz Eduardo <l.eduardos.lopes@gmail.com>
2022-10-27 19:43:33 -0300

    fix connection

--- api/database/conection.js
+++ api/database/conection.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize');
 // instancia do banco de dados  ( databasee | user | password )
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     dialect: 'postgres',
-    // port: 5431
+    host:"containers-us-west-52.railway.app",
+    port: 5571
   });
 
 module.exports = sequelize
